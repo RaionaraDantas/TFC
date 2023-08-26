edc8dd024784aa0d867748107aa6737f8be9b959
RaionaraDantas <raionara.dantas@gmail.com>
2023-08-25 23:37:31 -0300

    1

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,44 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Teams extends Model<InferAttributes<Teams>,
+InferCreationAttributes<Teams>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: DataTypes.STRING,
+}, {
+  sequelize: db,
+  modelName: 'Teams',
+  tableName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Teams;
