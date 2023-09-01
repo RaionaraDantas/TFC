ec33f6e3b2e8aee9ce7ebee26f999e355c0fd558
RaionaraDantas <raionara.dantas@gmail.com>
2023-08-31 22:55:43 -0300

    13

---
+++ app/backend/src/database/models/SequelizeMatchesModel.ts
@@ -0,0 +1,47 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+import Teams from './SequelizeTeamsModel';
+// import OtherModel from './OtherModel';
+
+class Matches extends Model<InferAttributes<Matches>,
+InferCreationAttributes<Matches>> {
+  declare id: CreationOptional<number>;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: number;
+}
+
+Matches.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeamId: DataTypes.INTEGER,
+  homeTeamGoals: DataTypes.INTEGER,
+  awayTeamId: DataTypes.INTEGER,
+  awayTeamGoals: DataTypes.INTEGER,
+  inProgress: DataTypes.INTEGER,
+}, {
+  sequelize: db,
+  modelName: 'User',
+  tableName: 'users',
+  timestamps: false,
+});
+
+Teams.hasMany(Matches, { foreignKey: 'home_team_id' });
+Teams.hasMany(Matches, { foreignKey: 'away_team_id' });
+
+Matches.belongsTo(Teams, { foreignKey: 'home_team_id', as: 'homeTeam' });
+Matches.belongsTo(Teams, { foreignKey: 'away_team_id', as: 'awayTeam' });
+
+export default Matches;
